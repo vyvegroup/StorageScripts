e516328d37048064fb10cf6d5430ef9e723d85d6
vyvegroup <cuaven137@gmail.com>
2026-03-24 23:49:00 +0700

    Add test-super-z.lua

---
+++ scripts/test-super-z.lua
@@ -0,0 +1,11 @@
+-- Test script from Super Z
+print("Hello from Super Z!")
+
+-- Simple function
+local function greet(name)
+    print("Welcome, " .. name .. "!")
+end
+
+greet("User")
+
+return "Script executed successfully!"
